3982357643073f9c6ad2ec61348042f2ebdca46d
Christian Carnevale <cncarnevale@gmail.com>
2022-05-07 18:22:56 -0400

    added .sql files index.js connection.js

---
+++ db/db.sql
@@ -0,0 +1,5 @@
+DROP DATABASE IF EXISTS bestpeople_db;
+
+CREATE DATABASE bestpeople_db;
+
+USE bestpeople_db;
--- db/schema.sql
+++ db/schema.sql
@@ -1,30 +1,29 @@
-DROP DATABASE IF EXISTS employees;
-CREATE DATABASE employees;
+DROP TABLE IF EXISTS department;
 
-USE employees;
+DROP TABLE IF EXISTS role;
+
+DROP TABLE IF EXISTS employee;
 
 CREATE TABLE department (
-  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) UNIQUE NOT NULL
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30) UNIQUE NOT NULL,
-  salary DECIMAL UNSIGNED NOT NULL,
-  department_id INT UNSIGNED NOT NULL,
-  INDEX dep_ind (department_id),
-  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INTEGER NOT NULL,
+    constraint department_fk FOREIGN KEY (department_id) REFERENCES department (id)
+
 );
 
 CREATE TABLE employee (
-  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT UNSIGNED NOT NULL,
-  INDEX role_ind (role_id),
-  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
-  manager_id INT UNSIGNED,
-  INDEX man_ind (manager_id),
-  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+  role_id INTEGER NOT NULL,
+  constraint employee_fk FOREIGN KEY (role_id) REFERENCES role (id),
+  manager_id INTEGER,
+  constraint manager_fk FOREIGN KEY (manager_id) REFERENCES employee (id)
 );
